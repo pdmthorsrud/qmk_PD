e575db2e958357ca96919c4e6b24d89aee1bea31
Per Daniel Thorsrud <happyotter@vpn-client155.uio.no>
2017-07-09 14:23:39 +0200

    Flyttet to knapper, that's it

--- keyboards/atreus/keymaps/PD/keymap.c
+++ keyboards/atreus/keymaps/PD/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_Q,            KC_W,     KC_E,       KC_R,                 KC_T,     KC_TRNS,              KC_Y,     KC_U,       KC_I,     KC_O,     KC_P    },
   {KC_A,            KC_S,     KC_D,       KC_F,                 KC_G,     KC_TRNS,              KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN },
   {KC_Z,            KC_X,     KC_C,       KC_V,                 KC_B,     KC_LALT,              KC_N,     KC_M,       KC_COMM,  KC_DOT,   NO_SLSH },
-  {MO(_FUNCTIONS),  KC_ESC,   KC_LGUI,    MT(MOD_LCTL, KC_ENT), KC_SPC,   MT(MOD_LSFT, KC_TAB), KC_BSPC,  MO(_CODE),  NO_MINS,  NO_QUES,  KC_LGUI  }
+  {MO(_FUNCTIONS),  KC_ESC,   KC_LGUI,    MOD_LSFT,             KC_SPC,   MT(MOD_LCTL, KC_ENT), KC_BSPC,  MO(_CODE),  NO_MINS,  NO_QUES,  KC_LGUI  }
 },
 /*
  *  !       @     up     {    }        ||     pgup    7     8     9    *

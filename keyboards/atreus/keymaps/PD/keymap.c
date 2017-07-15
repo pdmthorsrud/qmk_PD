d5fa8904f24105f55e355746b490e2c3aab0b300
Per Daniel Thorsrud <happyotter@Pers-MacBook-Pro.local>
2017-07-15 23:41:23 +0200

    Blabla

--- keyboards/atreus/keymaps/PD/keymap.c
+++ keyboards/atreus/keymaps/PD/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_TESTLAYER] = { /* [> FUNCTIONS <] */
   {RESET,     KC_AT,      NO_AE,    KC_LCBR,    KC_RCBR,    KC_TRNS, KC_PGUP,   KC_7,     KC_8,     NO_OSLH,          KC_TRNS},
-  {KC_ESC,    LCTL(LTAB), KC_MPLY,  KC_MNXT,    KC_DLR,     KC_TRNS, KC_PGDN,   KC_4,     KC_5,     KC_6,             KC_WAKE},
+  {KC_ESC,    LCTL(KC_TAB), KC_MPLY,  KC_MNXT,    KC_DLR,     KC_TRNS, KC_PGDN,   KC_4,     KC_5,     KC_6,             KC_WAKE},
   {KC_DELETE, KC_VOLD,    KC_VOLU,  KC_VOLU,    KC_AMPR,    KC_LALT, KC_GRV,    KC_1,     KC_2,     KC_3,             KC_SLEP},
   {KC_TRNS,   KC_INS,     KC_LGUI,  KC_TRNS,    KC_BSPC,    KC_TRNS, KC_SPC,    KC_TRNS,  KC_DOT,   KC_MYCM,          KC_PWR}
 }};

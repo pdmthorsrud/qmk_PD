518766e1577a29cf9d0fc01b947256dca767269b
pdthorsr <sagittarius@live.no>
2017-07-15 22:40:37 +0200

    heihei

--- keyboards/atreus/keymaps/PD/keymap.c
+++ keyboards/atreus/keymaps/PD/keymap.c
@@ -13,11 +13,12 @@
 #define _NOTHING 2
 #define _FUNCTIONS 3
 #define _GAMINGTG 4
+#define _TESTLAYER 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = { /* Qwerty */
   {KC_Q,            KC_W,     KC_E,       KC_R,                 KC_T,     KC_TRNS,              KC_Y,     KC_U,       KC_I,     KC_O,     KC_P    },
-  {KC_A,            KC_S,     KC_D,       KC_F,                 KC_G,     KC_TRNS,              KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN },
+  {KC_A,            KC_S,     KC_D,       LT(_TESTLAYER, KC_F), KC_G,     KC_TRNS,              KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN },
   {KC_Z,            KC_X,     KC_C,       KC_V,                 KC_B,     KC_LALT,              KC_N,     KC_M,       KC_COMM,  KC_DOT,   NO_SLSH },
   {MO(_FUNCTIONS),  KC_ESC,   KC_LGUI,    MOD_LSFT,             KC_SPC,   MT(MOD_LCTL, KC_ENT), KC_BSPC,  MO(_CODE),  NO_MINS,  NO_QUES,  KC_LGUI  }
 },
@@ -58,6 +59,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_2,            KC_A,     KC_S,       KC_D,                 KC_G,     KC_TRNS,              KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN },
   {KC_3,            KC_Z,     KC_X,       KC_C,                 KC_V,     KC_LALT,              MOD_LALT,     KC_TAB,       KC_COMM,  KC_DOT,   KC_SLSH },
   {KC_4,         KC_ESC,   KC_LGUI,    KC_TRNS,              KC_SPC,   KC_LCTL,              KC_BSPC,  MO(_CODE),  NO_MINS,  KC_QUOT,  KC_LGUI  }
+},
+
+[_TESTLAYER] = { /* [> FUNCTIONS <] */
+  {RESET,     KC_AT,      NO_AE,    KC_LCBR,    KC_RCBR,    KC_TRNS, KC_PGUP,   KC_7,     KC_8,     NO_OSLH,          KC_TRNS},
+  {KC_ESC,    LCTL(LTAB), KC_MPLY,  KC_MNXT,    KC_DLR,     KC_TRNS, KC_PGDN,   KC_4,     KC_5,     KC_6,             KC_WAKE},
+  {KC_DELETE, KC_VOLD,    KC_VOLU,  KC_VOLU,    KC_AMPR,    KC_LALT, KC_GRV,    KC_1,     KC_2,     KC_3,             KC_SLEP},
+  {KC_TRNS,   KC_INS,     KC_LGUI,  KC_TRNS,    KC_BSPC,    KC_TRNS, KC_SPC,    KC_TRNS,  KC_DOT,   KC_MYCM,          KC_PWR}
 }};
 
 const uint16_t PROGMEM fn_actions[] = {

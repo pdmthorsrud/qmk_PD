94a34372898302cd0cfc94163647498c342bfbc0
Per Daniel Thorsrud <happyotter@Pers-MacBook-Pro.local>
2017-07-17 15:41:41 +0200

    Lagt til nytt layer

--- keyboards/atreus/keymaps/PD/keymap.c
+++ keyboards/atreus/keymaps/PD/keymap.c
@@ -10,17 +10,17 @@
 // entirely and just use numbers.
 #define _QW 0
 #define _CODE 1
-#define _NOTHING 2
+#define _EXTRALETTERS 2
 #define _FUNCTIONS 3
 #define _GAMINGTG 4
-#define _TESTLAYER 5
+#define _QUICKACCESS 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = { /* Qwerty */
-  {KC_Q,            KC_W,     KC_E,       KC_R,                 KC_T,     KC_TRNS,              KC_Y,     KC_U,       KC_I,     KC_O,     KC_P    },
-  {KC_A,            KC_S,     KC_D,       LT(_TESTLAYER, KC_F), KC_G,     KC_TRNS,              KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN },
-  {KC_Z,            KC_X,     KC_C,       KC_V,                 KC_B,     KC_LALT,              KC_N,     KC_M,       KC_COMM,  KC_DOT,   NO_SLSH },
-  {MO(_FUNCTIONS),  KC_ESC,   KC_LGUI,    MOD_LSFT,             KC_SPC,   MT(MOD_LCTL, KC_ENT), KC_BSPC,  MO(_CODE),  NO_MINS,  NO_QUES,  KC_LGUI  }
+  {KC_Q,            KC_W,     KC_E,               KC_R,                   KC_T,     KC_TRNS,              KC_Y,     KC_U,       KC_I,     KC_O,     KC_P    },
+  {KC_A,            KC_S,     KC_D,               LT(_QUICKACCESS, KC_F), KC_G,     KC_TRNS,              KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN },
+  {KC_Z,            KC_X,     KC_C,               KC_V,                   KC_B,     KC_LALT,              KC_N,     KC_M,       KC_COMM,  KC_DOT,   NO_SLSH },
+  {MO(_FUNCTIONS),  KC_ESC,   OSL(_EXTRALETTERS), MT(MOD_LSFT, KC_TAB),   KC_SPC,   MT(MOD_LCTL, KC_ENT), KC_BSPC,  MO(_CODE),  NO_MINS,  NO_QUES,  KC_LGUI  }
 },
 /*
  *  !       @     up     {    }        ||     pgup    7     8     9    *
@@ -30,42 +30,57 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_CODE] = { /* [> RAISE <] */
   {KC_EXLM, KC_AT,   KC_UP,   NO_LCBR, NO_RCBR, KC_TRNS,                          KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR},
-  {NO_AA,   KC_LEFT, KC_DOWN, KC_RGHT, NO_PIPE, KC_TRNS,                          KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS},
-  {NO_LBRC, NO_RBRC, NO_LPRN, NO_RPRN, NO_AMPR, KC_TRNS,                          KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS},
+  {NO_AA,   KC_LEFT, KC_DOWN, KC_RGHT, NO_PIPE, KC_TRNS,                          KC_PGDN, KC_4,    KC_5,   KC_6, KC_TAB},
+  {NO_LBRC, NO_RBRC, NO_LPRN, NO_RPRN, NO_AMPR, KC_TRNS,                          KC_GRV,  KC_1,    KC_2,   KC_3, KC_TRNS},
   {KC_CAPS, KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, MT(MOD_LCTL | MOD_LSFT, KC_ENT),  KC_SPC,  KC_TRNS, KC_DOT, KC_0, NO_EQL}
 },
 /*
- * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
- *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
- *       volup             reset      ||             F1    F2    F3   F12
- *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
+ *  !       @     up     {    }        ||     pgup    7     8     9    *
+ *  #     left   down  right  |        ||     pgdn    4     5     6    +
+ *  [       ]      (     )    &        ||       `     1     2     3    \
+ * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
  */
-[_NOTHING] = { /* [> LOWER <] */
-  {KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_UP,   KC_F7,   KC_F8,   KC_TRNS,   KC_F10},
-  {KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_TRNS, KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11},
-  {KC_TRNS, KC_VOLU, KC_TRNS, KC_TRNS, RESET,   KC_LALT, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F12},
-  {KC_TRNS, KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS}
+[_EXTRALETTERS] = { /* [> LOWER <] */
+  {KC_TRNS, KC_TRNS, NO_AE,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LSFT(KC_I),  NO_OSLH, KC_TRNS},
+  {NO_AA,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS},
+  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_QW), KC_TRNS,     KC_TRNS, KC_TRNS}
 },
-
+/*
+ *  !       @     up     {    }        ||     pgup    7     8     9    *
+ *  #     left   down  right  |        ||     pgdn    4     5     6    +
+ *  [       ]      (     )    &        ||       `     1     2     3    \
+ * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
+ */
 [_FUNCTIONS] = { /* [> FUNCTIONS <] */
-  {RESET,   KC_AT,      NO_AE,    KC_LCBR,    KC_RCBR,    KC_TRNS, KC_PGUP,   KC_7,     KC_8,     NO_OSLH,     TG(_GAMINGTG)},
-  {KC_ESC,   KC_MPRV,   KC_MPLY,  KC_MNXT,   KC_DLR,     KC_TRNS, KC_PGDN,   KC_4,     KC_5,     KC_6,             KC_WAKE},
+  {RESET,     KC_AT,      NO_AE,    KC_LCBR,    KC_RCBR,    KC_TRNS, KC_PGUP,   KC_7,     KC_8,     NO_OSLH,          TG(_GAMINGTG)},
+  {KC_ESC,    KC_MPRV,    KC_MPLY,  KC_MNXT,    KC_DLR,     KC_TRNS, KC_PGDN,   KC_4,     KC_5,     KC_6,             KC_WAKE},
   {KC_DELETE, KC_VOLD,    KC_VOLU,  KC_VOLU,    KC_AMPR,    KC_LALT, KC_GRV,    KC_1,     KC_2,     KC_3,             KC_SLEP},
   {KC_TRNS,   KC_INS,     KC_LGUI,  KC_TRNS,    KC_BSPC,    KC_TRNS, KC_SPC,    KC_TRNS,  KC_DOT,   KC_MYCM,          KC_PWR}
 },
-
+/*
+ *  !       @     up     {    }        ||     pgup    7     8     9    *
+ *  #     left   down  right  |        ||     pgdn    4     5     6    +
+ *  [       ]      (     )    &        ||       `     1     2     3    \
+ * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
+ */
 [_GAMINGTG] = { /* GAMING */
   {KC_1,            KC_Q,     KC_W,       KC_E,                 KC_T,     KC_TRNS,              KC_Y,     KC_U,       KC_I,     KC_O,     KC_TRNS    },
   {KC_2,            KC_A,     KC_S,       KC_D,                 KC_G,     KC_TRNS,              KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN },
-  {KC_3,            KC_Z,     KC_X,       KC_C,                 KC_V,     KC_LALT,              MOD_LALT,     KC_TAB,       KC_COMM,  KC_DOT,   KC_SLSH },
-  {KC_4,         KC_ESC,   KC_LGUI,    KC_TRNS,              KC_SPC,   KC_LCTL,              KC_BSPC,  MO(_CODE),  NO_MINS,  KC_QUOT,  KC_LGUI  }
+  {KC_3,            KC_Z,     KC_X,       KC_C,                 KC_V,     KC_LALT,              MOD_LALT, KC_TAB,     KC_COMM,  KC_DOT,   KC_SLSH },
+  {KC_4,            KC_ESC,   KC_LGUI,    KC_TRNS,              KC_SPC,   KC_LCTL,              KC_BSPC,  MO(_CODE),  NO_MINS,  KC_QUOT,  KC_LGUI  }
 },
-
-[_TESTLAYER] = { /* [> FUNCTIONS <] */
-  {RESET,     KC_AT,      NO_AE,    KC_LCBR,    KC_RCBR,    KC_TRNS, KC_PGUP,   KC_7,     KC_8,     NO_OSLH,          KC_TRNS},
-  {KC_ESC,    LCTL(KC_TAB), KC_MPLY,  KC_MNXT,    KC_DLR,     KC_TRNS, KC_PGDN,   KC_4,     KC_5,     KC_6,             KC_WAKE},
-  {KC_DELETE, KC_VOLD,    KC_VOLU,  KC_VOLU,    KC_AMPR,    KC_LALT, KC_GRV,    KC_1,     KC_2,     KC_3,             KC_SLEP},
-  {KC_TRNS,   KC_INS,     KC_LGUI,  KC_TRNS,    KC_BSPC,    KC_TRNS, KC_SPC,    KC_TRNS,  KC_DOT,   KC_MYCM,          KC_PWR}
+/*
+ *  !       @     up     {    }        ||     pgup    7     8     9    *
+ *  #     left   down  right  |        ||     pgdn    4     5     6    +
+ *  [       ]      (     )    &        ||       `     1     2     3    \
+ * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
+ */
+[_QUICKACCESS] = { /* [> QUICKACCESS <] */
+  {RESET,     LALT(LSFT(KC_TAB)), LALT(KC_TAB),           KC_LCBR,    KC_RCBR,    KC_TRNS, KC_PGUP,   KC_7,     KC_8,     NO_OSLH,          KC_TRNS},
+  {NO_AA,     LCTL(LSFT(KC_TAB)), LCTL(KC_TAB),           KC_TRNS,    KC_DLR,     KC_TRNS, KC_PGDN,   KC_4,     KC_5,     KC_6,             KC_WAKE},
+  {KC_DELETE, KC_VOLD,            KC_VOLU,                KC_VOLU,    KC_AMPR,    KC_LALT, KC_GRV,    KC_1,     KC_2,     KC_3,             KC_SLEP},
+  {KC_TRNS,   KC_INS,             KC_LGUI,                KC_TRNS,    KC_BSPC,    KC_TRNS, KC_SPC,    KC_TRNS,  KC_DOT,   KC_MYCM,          KC_PWR}
 }};
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -86,3 +101,13 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       }
     return MACRO_NONE;
 };
+
+
+
+
+//[_EXTRALETTERS] = { /* [> LOWER <] */
+//  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+//  {NO_AA,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+//  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+//  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_QW), KC_TRNS, KC_TRNS, KC_TRNS}
+//},

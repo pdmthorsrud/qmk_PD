e0dc6f36aa127f3e0f48f958ac11fda17d65807d
Per Daniel Thorsrud <pdthorsr@student.sv.uio.no>
2019-09-19 21:56:52 +0200

    cant remember

--- keyboards/kb/keymaps/default/keymap.c
+++ keyboards/kb/keymaps/default/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_9,    KC_1,     KC_1,     KC_2,     KC_3,     KC_4,    KC_5,     KC_6,     KC_7,      	KC_8,       KC_9,    KC_0,     NO_PLUS,  KC_TRNS,
   		KC_8,    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,     KC_Y,     KC_U,      	KC_I,       KC_O,    KC_P,     NO_AA,    NO_LESS,
 		KC_7,    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,    KC_G,     KC_H,     KC_J,      	KC_K,       KC_L,    NO_OSLH,  NO_AE,    NO_AT,
-		KC_6,    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,     KC_N,     KC_M,      	KC_COMM,    KC_DOT,  KC_MINS,  KC_UP,    KC_N,
+		KC_6,    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,     KC_N,     KC_M,      	KC_COMM,    KC_DOT,  NO_MINS,  KC_UP,    KC_N,
 		KC_5,    MO(3),    KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,  KC_BSPC,  KC_BSPC,  LT(MO(2),KC_ENTER), KC_ENT,  KC_LGUI,  KC_LEFT,  KC_DOWN,   KC_RGHT),
 
 	KEYMAP(
